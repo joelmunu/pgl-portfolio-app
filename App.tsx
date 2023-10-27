d6d60e237af8180d0ca4b48228ad7364e624a5b8
joelmunu <joel.munu@gmail.com>
2023-10-27 13:11:49 +0100

    Refactor: Se ha creado el componente HobbiesList y se ha refactorizado el codigo del mismo

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import QRCode from "react-native-qrcode-svg";
 import Header from "./components/Header";
 import { useState } from "react";
 import Description from "./components/Description";
+import HobbiesList from "./components/HobbiesList";
 
 export default function App() {
   const [displayMyQR, setDisplayMyQR] = useState(true);
@@ -13,47 +14,7 @@ export default function App() {
       {displayMyQR ? (
         <View style={styles.bodystails}>
           <Description />
-          <View>
-            <Text
-              style={{
-                color: "beriblak",
-                fontWeight: "900",
-                textTransform: "capitalize",
-                fontSize: 20,
-                textAlign: "center",
-              }}
-            >
-              cosas que me gustan mucho:
-            </Text>
-            <ScrollView style={{ padding: 10 }}>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>
-                Salir a pasear
-              </Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Senderismo</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>
-                Ir a la playita
-              </Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>
-                Domingos de misa
-              </Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>
-                La guitarrita
-              </Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>
-                El monte con lluvia
-              </Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Viajar</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>
-                Música variadita
-              </Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Anime</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Ducharme</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>Videojuegos</Text>
-              <Text style={styles.cosasQmeGustanMuxoEstails}>
-                Ir de cenar romántica
-              </Text>
-            </ScrollView>
-          </View>
+          <HobbiesList />
         </View>
       ) : (
         <View style={styles.bodystails}>
@@ -101,18 +62,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "space-between",
     height: "85%",
-  },
-  cosasQmeGustanMuxoEstails: {
-    borderColor: "black",
-    borderWidth: 1,
-    borderStyle: "dashed",
-    padding: 20,
-    color: "darkred",
-    textAlign: "center",
-    fontWeight: "bold",
-    fontStyle: "italic",
-    fontSize: 16,
-    backgroundColor: "silver",
   },
   CentrarcodigoQR: {
     justifyContent: "center",
---
+++ components/HobbiesList.tsx
@@ -0,0 +1,47 @@
+import { StyleSheet, Text, View, ScrollView } from 'react-native'
+
+const HobbiesList = () => {
+    return (
+        <View>
+            <Text style={styles.titleStyle}>cosas que me gustan mucho:</Text>
+            <ScrollView style={{ padding: 10 }}>
+                <Text style={styles.hobbiesStyle}>Salir a pasear</Text>
+                <Text style={styles.hobbiesStyle}>Senderismo</Text>
+                <Text style={styles.hobbiesStyle}>Ir a la playita</Text>
+                <Text style={styles.hobbiesStyle}>Domingos de misa</Text>
+                <Text style={styles.hobbiesStyle}>La guitarrita</Text>
+                <Text style={styles.hobbiesStyle}>El monte con lluvia</Text>
+                <Text style={styles.hobbiesStyle}>Viajar</Text>
+                <Text style={styles.hobbiesStyle}>Música variadita</Text>
+                <Text style={styles.hobbiesStyle}>Anime</Text>
+                <Text style={styles.hobbiesStyle}>Ducharme</Text>
+                <Text style={styles.hobbiesStyle}>Videojuegos</Text>
+                <Text style={styles.hobbiesStyle}>Ir de cenar romántica</Text>
+            </ScrollView>
+        </View>
+    )
+}
+
+export default HobbiesList
+
+const styles = StyleSheet.create({
+    hobbiesStyle: {
+        borderColor: "black",
+        borderWidth: 1,
+        borderStyle: "dashed",
+        padding: 20,
+        color: "darkred",
+        textAlign: "center",
+        fontWeight: "bold",
+        fontStyle: "italic",
+        fontSize: 16,
+        backgroundColor: "silver",
+    },
+    titleStyle: {
+        color: "black",
+        fontWeight: "900",
+        textTransform: "capitalize",
+        fontSize: 20,
+        textAlign: "center",
+    }
+})
